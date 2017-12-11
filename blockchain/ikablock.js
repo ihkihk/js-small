3d066c0050fafbfbe2dcaecfa1391e706db856c7
xery <ivokassamakov@gmail.com>
2017-12-11 01:13:32 +0100

    Update

--- blockchain/ikablock.js
+++ blockchain/ikablock.js
@@ -1,34 +1,41 @@
-//const SHA256 = require('crypto/sha256');
+const crypto = require('crypto');
+
 
 class Block{
     constructor(index, timestamp, data, previousHash=''){
-        self.index = index;
-        self.timestamp = timestamp;
-        self.data = data;
-        self.previousHash = previousHash;
-        self.nonce = 0;
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.nonce = 0;
 
-        self.hash = "0"; // block not yet mined
+        this.hash = "-"; // block not yet mined
     }
 
     calculateHash() {
-        return SHA256(self.index + self.timestamp + 
-            JSON.stringify(self.data) + self.previousHash + 
-            self.nonce).toString();
+        const SHA256 = crypto.createHash('sha256');
+
+        return SHA256.update(this.index + this.timestamp + 
+            JSON.stringify(this.data) + this.previousHash + 
+            this.nonce).digest('hex');
     }
 
     mineBlock(difficulty) {
-        while (self.hash.substring(0, difficulty) != Array(difficulty).join("0")) {
-            self.nonce++;
-            self.hash = calculateHash();
+        const checkString = Array(difficulty+1).join("0");
+
+        while (this.hash.substring(0, difficulty) !== checkString) {
+            this.nonce++;
+            this.hash = this.calculateHash();
         }
+
+        console.log(this.hash);
     }
 }
 
 class Blockchain{
     constructor(){
-        self.chain = [self.createGenesisBlock()]
-        self.difficulty = 3;
+        this.chain = [this.createGenesisBlock()]
+        this.difficulty = 1;
     }
 
     createGenesisBlock(){
@@ -36,31 +43,36 @@ class Blockchain{
     }
 
     addBlock(blk){
-        blk.previousHash = self.chain[self.chain.length-1].hash;
-        blk.mineBlock(3);
-        self.chain.push(blk);
+        blk.previousHash = this.getLatestBlock().hash;
+        blk.mineBlock(this.difficulty);
+        this.chain.push(blk);
     }
 
     getLatestBlock() {
-        return self.chain[self.chain.length-1]
+        return this.chain[this.chain.length-1]
     }
 
     isChainValid(){
-        for (i=1; i < self.chain.length; i++){
-            curBlk = self.chain[i];
-            prevBlk = self.chain[i-1];
+        for (var i = 1; i < this.chain.length; i++){
+            const curBlk = this.chain[i];
+            const prevBlk = this.chain[i-1];
+
             if (curBlk.hash != curBlk.calculateHash())
                 return false;
+
             if (curBlk.previousHash != prevBlk.hash)
                 return false;
+
             return true;
         }
 
     }
 }
 
-var ivoCoin = new Blockchain();
+let ivoCoin = new Blockchain();
 
+console.log("Mining block 1 ....");
 ivoCoin.addBlock(new Block(1, "12/11/17", {amount:4}));
+console.log("Mining block 2 ....");
 ivoCoin.addBlock(new Block(2, "13/11/17", {amount:-14}));
 console.log("Is chain valid?: " + ivoCoin.isChainValid())
